91d34acd21d0ffb96e8b8da5afdb91b71dc06fce
kawagami <joelai1988@gmail.com>
2023-11-09 00:48:05 +0800

    update: layout 規劃中

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,18 @@
 </script>
 
 <template>
-  <div class="background"></div>
+  <!-- <div class="background">
+    <header>header</header>
+    <aside>aside</aside>
+    <main>main</main>
+    <footer>footer</footer>
+  </div> -->
+  <el-row class="background">
+    <el-col class="header" :xs="8" :sm="6" :md="4" :lg="3" :xl="1">header</el-col>
+    <el-col class="aside" :xs="8" :sm="6" :md="4" :lg="3" :xl="1">aside</el-col>
+    <el-col class="main" :xs="8" :sm="6" :md="4" :lg="3" :xl="1">main</el-col>
+    <el-col class="footer" :xs="8" :sm="6" :md="4" :lg="3" :xl="1">footer</el-col>
+  </el-row>
 </template>
 
 <style scoped lang="scss">
@@ -10,5 +21,21 @@
   width: 100%;
   height: 100vh;
   background-color: #ffa;
+
+  .header {
+    background-color: #d35252;
+  }
+
+  .aside {
+    background-color: #1b138b;
+  }
+
+  .main {
+    background-color: #fae;
+  }
+
+  .footer {
+    background-color: #90a;
+  }
 }
 </style>
