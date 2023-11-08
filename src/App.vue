58557d0fbfa72df2c955c77141b634b12026b81f
kawagami <joelai1988@gmail.com>
2023-11-09 00:11:56 +0800

    add: 履歷建置中

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="background"></div>
+</template>
+
+<style scoped lang="scss">
+.background {
+  width: 100%;
+  height: 100vh;
+  background-color: #ffa;
+}
+</style>
